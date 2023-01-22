2b01cfc72da7713f0f0c639429bd310748a14846
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-01-22 10:57:25 -0600

    ref vs reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,52 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-
+import { ref, reactive } from 'vue';
 export default {
   name: 'HomeView',
   components: {},
+  setup() {
+    const ironman = ref({ name: 'Tony', age: 50 });
+    const hulk = reactive({ name: 'Bruce', age: 50 });
+
+    const changeIronman = () => {
+      ironman.value.name = 'Ironman';
+      ironman.value.age = 21;
+    };
+    const changeHulk = () => {
+      hulk.name = 'Hulk';
+      hulk.age = 60;
+    };
+
+    const ironmanIsAlive = ref(false);
+    // TODO: BAD reactive no acepta primitivos, solo objetos
+    // const hulkIsAlive = reactive(false);
+
+    const changeStatus = () => {
+      ironmanIsAlive.value = !ironmanIsAlive.value;
+      // hulkIsAlive = !hulkIsAlive;
+    };
+
+    return {
+      ironman,
+      hulk,
+      ironmanIsAlive,
+
+      changeIronman,
+      changeHulk,
+      changeStatus,
+    };
+  },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ ironman }}</h1>
+    <h1>{{ hulk }}</h1>
+    <button @click="changeIronman">Change Ironman</button>
+    <button @click="changeHulk">Change Hulk</button>
+    <div>
+      <button @click="changeStatus">Ref vs Reactive</button>
+      <h2>Iron: {{ ironmanIsAlive }}</h2>
+    </div>
+  </div>
+</template>
