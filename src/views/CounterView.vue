e9287adab954bbfeab8b0d297f116c6296055d5c
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-01-22 08:48:15 -0600

    My first commit

---
+++ src/views/CounterView.vue
@@ -0,0 +1,32 @@
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'Counter',
+  props: {},
+  setup() {
+    const counter = ref(5);
+
+    // const increase = () => {
+    //   counter.value++;
+    // };
+    // const decrease = () => {
+    //   counter.value--;
+    // };
+    return {
+      counter,
+      increase: () => counter.value++,
+      decrease: () => counter.value--,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h2>Counter view</h2>
+    <h1>{{ counter }}</h1>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
